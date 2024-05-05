bb7d52ab468c93bfe87f5047ce35e44722ddd790
Julian Amano <juliansamano@gmail.com>
2024-05-04 22:52:58 -0400

    navbar link fix

--- src/app/components/ui/Navbar.tsx
+++ src/app/components/ui/Navbar.tsx
@@ -9,70 +9,65 @@ type propType = {
 export default function Navbar({ language }: propType) {
   const handleScroll = (e: any) => {
     e.preventDefault();
+    console.log(e.target);
     const elem = document.getElementById(e.target.value);
     elem?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <div className="flex justify-around bg-black text-white text-2xl py-2">
-      <button
-        value="schedule"
-        onClick={handleScroll}
-        className="relative flex justify-center"
-      >
-        <div
-          className={`absolute transition duration-1000 ${
-            language == "english" ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          Schedule
-        </div>
-        <div
-          className={`top-0 text-lg transition duration-1000 ${
-            murecho.className
-          } ${language == "english" ? "opacity-0" : "opacity-100"}`}
-        >
-          スケジュール
+      <button value="schedule" onClick={handleScroll}>
+        <div className="pointer-events-none flex justify-center">
+          <div
+            className={`absolute transition duration-1000 ${
+              language == "english" ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Schedule
+          </div>
+          <div
+            className={`top-0 text-lg transition duration-1000 ${
+              murecho.className
+            } ${language == "english" ? "opacity-0" : "opacity-100"}`}
+          >
+            スケジュール
+          </div>
         </div>
       </button>
-      <button
-        value="bio"
-        onClick={handleScroll}
-        className="relative flex justify-center"
-      >
-        <div
-          className={`absolute transition duration-1000 ${
-            language == "english" ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          Bio
-        </div>
-        <div
-          className={`top-0 text-lg transition duration-1000 ${
-            murecho.className
-          } ${language == "english" ? "opacity-0" : "opacity-100"}`}
-        >
-          プロフィール
+      <button value="bio" onClick={handleScroll}>
+        <div className="pointer-events-none flex justify-center">
+          <div
+            className={`absolute transition duration-1000 ${
+              language == "english" ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Bio
+          </div>
+          <div
+            className={`top-0 text-lg transition duration-1000 ${
+              murecho.className
+            } ${language == "english" ? "opacity-0" : "opacity-100"}`}
+          >
+            プロフィール
+          </div>
         </div>
       </button>
-      <button
-        value="blog"
-        onClick={handleScroll}
-        className="relative flex justify-center"
-      >
-        <div
-          className={`absolute transition duration-1000 ${
-            language == "english" ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          Blog
-        </div>
-        <div
-          className={`top-0 text-lg transition duration-1000 ${
-            murecho.className
-          }  ${language == "english" ? "opacity-0" : "opacity-100"}`}
-        >
-          ブログ
+      <button value="blog" onClick={handleScroll}>
+        <div className="pointer-events-none flex justify-center">
+          <div
+            className={`absolute transition duration-1000 ${
+              language == "english" ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            Blog
+          </div>
+          <div
+            className={`top-0 text-lg transition duration-1000 ${
+              murecho.className
+            }  ${language == "english" ? "opacity-0" : "opacity-100"}`}
+          >
+            ブログ
+          </div>
         </div>
       </button>
     </div>
