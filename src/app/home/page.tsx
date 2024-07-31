758bdb59773f94d442330238c846dbbb332c5147
Julian Amano <juliansamano@gmail.com>
2024-07-31 10:29:13 -0400

    revalidate 0 for homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,8 @@
 import HomePage from "../components/ui/HomePage";
 import prisma from "../../../lib/prisma";
 
+export const revalidate = 0;
+
 export default async function Home() {
   const bioData = await prisma.profile.findUnique({
     where: { id: "663e2ea55ccb92e4cd356ba6" },
